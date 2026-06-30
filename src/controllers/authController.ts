37d93733337e2dc9c1a7225509a9db93031ac460
fmughalishere <fizamuneer0101@gmail.com>
2026-06-30 16:14:54 +0500

    Updated

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,7 +7,7 @@ import { signToken } from "../utils/jwt";
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
+  secure: true,
   sameSite: "lax" as const,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
